5631dcd055d811af68ee7186ae6544e228782511
orihab <orielh@anyvision.co>
2019-11-05 17:20:40 +0200

    memsql: refactored connection property - no logic changes

--- infra/modules/memsql.py
+++ infra/modules/memsql.py
@@ -9,33 +9,35 @@ from infra.model import plugins
 
 class Memsql(object):
     def __init__(self, host, memsql_host=CONSTS.MEMSQL, memsql_port=CONSTS.MEMSQL_PORT):
-        # self._host = host
-        # self._memsql_host = memsql_host
-        # self._memsql_port = memsql_port
         self.tunnel = sshtunnel.open_tunnel(host.ip,
                                    ssh_username=host.user, ssh_password=host.password,
                                    remote_bind_address=(memsql_host, memsql_port))
         self.tunnel.start()
+        self._connection = None
+
+    @property
+    def connection(self):
+        if self._connection is None:
+            self.connection = self._get_connection()
+        return self.connection
 
     def _get_connection(self):
         connection = pymysql.connect(host='localhost',
-                                         port=self.tunnel.local_bind_port,
-                                         user='root',
-                                         password='password',
-                                         cursorclass=pymysql.cursors.DictCursor)
+                                     port=self.tunnel.local_bind_port,
+                                     user='root',
+                                     password='password',
+                                     cursorclass=pymysql.cursors.DictCursor)
 
         return connection
 
     def upsert(self, query):
-        conn = self._get_connection()
-        with closing(conn.cursor()) as cursor:
+        with closing(self.connection.cursor()) as cursor:
             res = cursor.execute(query)
-        conn.commit()
+        self.connection.commit()
         return res
 
     def fetch_all(self, query):
-        conn = self._get_connection()
-        with closing(conn.cursor(pymysql.cursors.DictCursor)) as cursor:
+        with closing(self.connection.cursor(pymysql.cursors.DictCursor)) as cursor:
             cursor.execute(query)
             res = cursor.fetchall()
         return res
