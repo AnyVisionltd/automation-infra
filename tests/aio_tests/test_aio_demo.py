ef6281d39f4cea55d8c553fc9eeca1a517f724a6
orihab <orielh@anyvision.co>
2019-11-24 13:41:05 +0200

    test_aio_demo: updates to account for changed fixture

--- tests/aio_tests/test_aio_demo.py
+++ tests/aio_tests/test_aio_demo.py
@@ -18,70 +18,70 @@ from runner.helpers import hardware_config
 
 # These are all example tests:
 @hardware_config(hardware={"type": "aio"})
-def test_ssh(host):
+def test_ssh(base_config):
     print("Running ssh test!")
     os.system("echo this is a test > /tmp/temp.txt")
-    host.SSH.put('/tmp/temp.txt', '/tmp')
-    res = host.SSH.execute('ls /tmp')
+    base_config.SSH.put('/tmp/temp.txt', '/tmp')
+    res = base_config.SSH.execute('ls /tmp')
     assert 'temp.txt' in res.split()
-    host.SSH.execute('rm /tmp/temp.txt')
-    res = host.SSH.execute('ls /tmp')
+    base_config.SSH.execute('rm /tmp/temp.txt')
+    res = base_config.SSH.execute('ls /tmp')
     assert 'temp.txt' not in res.split()
 
 
 @hardware_config(hardware={"type": "aio"})
-def test_s3(host):
-    host.Seaweed.create_bucket('test_bucket')
-    content = host.Seaweed.get_buckets()
+def test_s3(base_config):
+    base_config.Seaweed.create_bucket('test_bucket')
+    content = base_config.Seaweed.get_buckets()
     assert b'test_bucket' in content
-    host.Seaweed.delete_bucket('test_bucket')
-    content = host.Seaweed.get_buckets()
+    base_config.Seaweed.delete_bucket('test_bucket')
+    content = base_config.Seaweed.get_buckets()
     assert b'test_bucket' not in content
 
 
 @hardware_config(hardware={"type": "aio"})
-def test_memsql_add_suspect(host):
+def test_memsql_add_suspect(base_config):
     poi_id = random.randint(0, 999999)
     query = f'''INSERT INTO `reid_db`.`poi`
             (`poi_id`,`detection_type`,`is_ignored`,`feature_id`,`features`,`valid_until`)
             VALUES
             ({poi_id},1,0,123,123,4141);'''
-    res = host.Memsql.upsert(query)
+    res = base_config.Memsql.upsert(query)
     assert res == 1
 
 
 @hardware_config(hardware={"type": "aio"})
-def test_pipeng_features(host):
-    response = host.PipeNg.health_check()
+def test_pipeng_features(base_config):
+    response = base_config.PipeNg.health_check()
     assert response.status == 1
 
 
 @hardware_config(hardware={"type": "aio"})
-def test_consul_get_services(host):
-    _, services_dict = host.Consul.get_services()
+def test_consul_get_services(base_config):
+    _, services_dict = base_config.Consul.get_services()
     assert len(services_dict) > 0
     assert 'camera-service' in services_dict
     put_key, put_val = ("test_key", "test_val")
-    res = host.Consul.put_key(put_key, put_val)
+    res = base_config.Consul.put_key(put_key, put_val)
     assert res is True
-    val = host.Consul.get_key("test_key")
+    val = base_config.Consul.get_key("test_key")
     assert val.decode('utf-8') == put_val
 
 
 @hardware_config(hardware={"type": "aio"})
-def test_kafka_functionality(host):
-    topics = host.Kafka.get_topics()
+def test_kafka_functionality(base_config):
+    topics = base_config.Kafka.get_topics()
     assert len(topics.topics) > 0
-    success = host.Kafka.create_topic('oris_new_topic')
+    success = base_config.Kafka.create_topic('oris_new_topic')
     assert success
     time.sleep(5)
-    success = host.Kafka.delete_topic('oris_new_topic')
+    success = base_config.Kafka.delete_topic('oris_new_topic')
     assert success
 
 
 @hardware_config(hardware={"type": "aio"})
-def test_camera_service(host):
-    cameras = host.CameraService.get_cameras()
+def test_camera_service(base_config):
+    cameras = base_config.CameraService.get_cameras()
     print(cameras)
     assert 1
 
