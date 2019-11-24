57bee8aaabeb98e24978137e9416f2e216e6bc86
orihab <orielh@anyvision.co>
2019-11-24 13:39:09 +0200

    aio_tests/conftest: fixture now connects via ssh and inits an ssh container
    Then uses only the connection to the container which is running ssh
    inside the HUT. This way we will have a container which we know has
    installed any dependencies we need which are irrelevant of which OS the
    HUT is running.

--- tests/aio_tests/conftest.py
+++ tests/aio_tests/conftest.py
@@ -4,32 +4,60 @@ from munch import *
 import pytest
 
 from infra.model.host import Host
+from infra.model import host as host_module
+from runner import CONSTS
 
 
 def pytest_addoption(parser):
     parser.addoption(
         "--sut_config",
         action="store",
-        default="""
-        {
-            "ip": "192.168.20.34",
-            "user": "user",
-            "password": "pass",
-            "key_file_path": "",
-            "alias": "monster",
-            "host_id": 123,
-            "host_type": "physical",
-            "allocation_id": ""
+        default="""{ "host":
+            {
+                "ip": "192.168.21.163",
+                "user": "root",
+                "password": "",
+                "key_file_path": "/home/ori/Projects/automation-infra/runner/docker_build/docker_user.pem",
+                "alias": "monster",
+                "host_id": 123,
+                "host_type": "physical",
+                "allocation_id": ""
+            }
         }
         """,
         help="ip user/pass config details",
     )
 
 
+def run_proxy_container(connected_ssh_module):
+    res = connected_ssh_module.execute('mkdir -p /tmp/build')
+    res = connected_ssh_module.put('./runner/docker_build/Dockerfile', '/tmp/build')
+    res = connected_ssh_module.put('./runner/docker_build/docker-compose.yml', '/tmp/build')
+    res = connected_ssh_module.execute('docker build -t automation-tests:0.1 /tmp/build')
+    res = connected_ssh_module.execute('docker-compose -f /tmp/build/docker-compose.yml up -d')
+    print(res)
+
+
+def remove_proxy_container(connected_ssh_module):
+    res = connected_ssh_module.execute('docker-compose -f /tmp/build/docker-compose.yml down')
+    print(res)
+
+
 @pytest.fixture(scope='session')
-def host(request):
+def base_config(request):
     config = json.loads(request.config.getoption('--sut_config'))
-    host = Host(Munch.fromDict(config))
-    # TODO: init communication docker container
+    host = Host(Munch.fromDict(config['host']))
+    # The SSH and the SSH_host dont need to be separate, they can just be initialized on diff ports (default is 22)..
+    host.SSH.connect()
+    run_proxy_container(host.SSH)
+    host.SSH.connect(CONSTS.TUNNEL_PORT)
     # TODO: switch if working in docker/k8s
-    return host
+    yield host
+    host.SSH.connect()
+    remove_proxy_container(host.SSH)
+
+
+if __name__ == '__main__':
+    h = host_module.init_example_host()
+    h.SSH.connect()
+    run_proxy_container(h.SSH)
