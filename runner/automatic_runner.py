7e3f26919c95bf3e70fa883fc5e8a5fa6681f02e
orihab <orielh@anyvision.co>
2019-12-01 15:06:26 +0200

    logging: to file instead of to stdout

--- runner/automatic_runner.py
+++ runner/automatic_runner.py
@@ -5,9 +5,16 @@ from collections import defaultdict
 from pprint import pprint
 import pytest
 import munch
+import logging
 
 from runner import hardware_initializer
 
+def init_logger():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(relativeCreated)6d %(threadName)s %(message)s',
+                        filename='output.log')
+
+init_logger()
 
 def run_test_module(module_name, test_name=None):
     module_results = {module_name: {}}
@@ -18,15 +25,17 @@ def run_test_module(module_name, test_name=None):
     for name, test in module_tests:
         if (test_name is None) or (name == test_name):
             test_hardware = test.__hardware_reqs
-            print(f"{name} hardware reqs:")
+            logging.info(f"{name} hardware reqs:")
             pprint(test_hardware)
             hardware_config = hardware_initializer.init_hardware(test_hardware)
-            print("running pytest...")
+            logging.info("running pytest...")
             sut_config = f'--sut_config={hardware_config}'
-            print(f"sut_config: {sut_config}")
+            logging.info(f"sut_config: {sut_config}")
             test_path = '/'.join(module_name.split('.'))
             test_names.append(name)
-    res = pytest.main(['-s', *[f"./tests/{test_path}.py::{name}" for name in test_names], '-n 1', sut_config])
+    pytest_args = ['-s', *[f"./tests/{test_path}.py::{name}" for name in test_names], '-n 1', sut_config]
+    #pytest_args.append('--log-file=output.log')
+    res = pytest.main(pytest_args)
     return res
     #return munch.DefaultFactoryMunch.fromDict(module_results, munch.DefaultFactoryMunch)
 
@@ -42,8 +51,8 @@ def sample_module_run():
 
     results_munch = munch.DefaultFactoryMunch.fromDict(all_results, munch.DefaultFactoryMunch)
 
-    print('--------ALL TEST RESULTS----------')
-    print(f"RUNTIME: {runtime}")
+    logging.info('--------ALL TEST RESULTS----------')
+    logging.info(f"RUNTIME: {runtime}")
     pprint(results_munch)
 
 
