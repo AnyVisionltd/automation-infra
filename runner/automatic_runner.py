d3a1126628d57c15cf80a64a741a560d2345c6d6
orihab <orielh@anyvision.co>
2019-12-15 13:11:53 +0200

    automatic_runner: runs tests based on 1 pytest call and inits_hardware in pytest hook
    This is much better than previously I ran pytest to collect the tests
    and then I had to init_hardware, and then I had to re-run pytest with
    inited hardware_setup. This way a pytest hook jumps in after collecting
    the tests and inits hardware on its own. Much better.

--- runner/automatic_runner.py
+++ runner/automatic_runner.py
@@ -15,32 +15,11 @@ from runner import helpers
 helpers.init_logger()
 
 
-def collect_tests_and_init_hardware(module_name):
-    collector = TestCollector()
-    pytest.main(['--collect-only', module_name], plugins=[collector])
-
-    tests = collector.collected
-    for test in tests:
-        logging.info(f"initing hardware for test {test.nodeid}")
-        test.hardware_config = hardware_initializer.init_hardware(test.obj.__hardware_reqs)
-    return tests
-
-
 def run_test_module(module_name):
-    # TODO: I dont like how this runs pytest twice. There has got to be a way to collect, init_hardware and then continue
-    #  running the already collected tests. look up pytest hooks or somn.
-    logging.info("collecting tests and initing hardware..")
-    initialized_tests = collect_tests_and_init_hardware(module_name)
-    logging.info("done collecting and initing...")
-    tests_config = [[f'{os.path.dirname(test.fspath.strpath)}/{os.path.basename(test.nodeid)}', f'--sut_config={test.hardware_config}']
-                    for test in initialized_tests]
-    logging.info("running tests really...")
-    for idx, test_config in enumerate(tests_config):
-        pytest_args = ['-s', '-n 1', f'--html=report{idx}.html', '--self-contained-html']
-        test_config.extend(pytest_args)
-        logging.info(f"pytest args: {test_config}")
-        res = pytest.main(test_config)
-    logging.info("done running tests!")
+    # TODO: this needs to run in new process
+    pytest_args = [module_name, '-s', '-n 1', f'--html=report.html', '--self-contained-html']
+    logging.info(f"pytest args: {pytest_args}")
+    res = pytest.main(pytest_args)
     return res
 
 
