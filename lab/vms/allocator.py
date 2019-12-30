0a8c720d9405c831030b9b712c0a3051ebe900bb
Solganik Alexander <alexanders@anyvision.co>
2019-12-30 23:55:18 +0200

    vm: adding allocator class.
    Allocator is a resource manager for hypervisor. it is actually
    responsible for managing gpus and mac addresses for vms. it bookeeps vm
    allocations.
    
    It works in the following way:
     When request arrives at allocator it reserves resources from "mac" and
    "gpu" pool, in case vm creating fails it returns resoures to the pool.
    
    Currently we support 2 types of networks, isolated network which is a
    network behind NAT and bridged network, which is directly connected to
    actual hypervisor L2 via macvtap

---
+++ lab/vms/allocator.py
@@ -0,0 +1,96 @@
+import logging
+from lab import NotEnoughResourceException
+import munch
+
+
+class Allocator(object):
+
+    def __init__(self, mac_addresses, gpus_list, vm_manager, server_name, max_vms,
+                 sol_base_port, paravirt_device, private_network="default"):
+        self.mac_addresses = mac_addresses
+        self.gpus_list = gpus_list
+        self.vms = {}
+        self.vm_manager = vm_manager
+        self.server_name = server_name
+        self.max_vms = max_vms
+        self.paravirt_net_device = paravirt_device
+        self.private_network = private_network
+        self.sol_base_port = sol_base_port
+
+    def _sol_port(self):
+        return self.sol_base_port + len(self.vms)
+
+    def _reserve_gpus(self, num_gpus):
+        gpus = self.gpus_list[:num_gpus]
+        self.gpus_list = self.gpus_list[num_gpus:]
+        return gpus
+
+    def _reserve_macs(self, num_macs):
+        required_macs = self.mac_addresses[:len(num_macs)]
+        self.mac_addresses = self.mac_addresses[len(num_macs):]
+        return required_macs
+
+    def _reserve_networks(self, networks):
+        required_macs = self._reserve_macs(networks)
+
+        return [{"macaddress" : mac,
+                 "mode" : network_type,
+                 'source' : self.paravirt_net_device if network_type == 'bridge' else self.private_network}
+                for mac, network_type in zip(required_macs, networks)]
+
+    def _free_vm_resources(self, gpus, networks):
+        macs = [net['macaddress'] for net in networks]
+        self.gpus_list.extend(gpus)
+        self.mac_addresses.extend(macs)
+
+    async def allocate_vm(self, base_image, memory_gb, networks, num_gpus=0, num_cpus=4):
+        ''' 
+        @networks - list of networks that we want to allocate, possible 
+        values are "isolated, bridged"
+        @num_gpus - number of GPU`s to allocate 
+        @num_cpus - number of CPU`s to allocate
+        @memory_gb - memory in GB for vm
+        @networks - list of networks types to allocate, possible values are 'bridge', 'NAT'
+        '''
+        logging.debug("Allocate vm image %(base_image)s memory %(memory_gb)s networks %(networks)s cpus %(num_cpus)s gpus %(num_gpus)s",
+                      dict(base_image=base_image, memory_gb=memory_gb, num_gpus=num_gpus, num_cpus=num_cpus, networks=networks))
+
+        # check that i have enough networks in pool 
+        if len(networks) > len(self.mac_addresses):
+            raise NotEnoughResourceException(f"Not nrough mac addresses in pool requested: {networks} has {self.mac_addresses}")
+        # Check that i have enough gpus 
+        if num_gpus > len(self.gpus_list):
+            raise NotEnoughResourceException(f"Not enough gpus requested : {num_gpus} has {self.gpus_list}")
+
+        if self.max_vms == len(self.vms):
+            raise NotEnoughResourceException(f"Cannot allocate more vms currently {self.vms}")
+
+        gpus = self._reserve_gpus(num_gpus)
+        networks = self._reserve_networks(networks)
+        vm_name = "%s-vm-%d" % (self.server_name, len(self.vms))
+        vm = munch.Munch(name=vm_name, num_cpus=num_cpus, memsize=memory_gb,
+                         net_ifaces=networks, sol_port=self._sol_port(),
+                         pcis=gpus, base_image=base_image)
+        self.vms[vm_name] = vm
+
+        try:
+            await self.vm_manager.allocate_vm(vm)
+        except:
+            self._free_vm_resources(gpus, networks)
+            del self.vms[vm_name]
+            raise
+        else:
+            logging.info("Allocated vm {vm}")
+        return vm
+
+    async def destroy_vm(self, name):
+        vm = self.vms[name]
+        try:
+            await self.vm_manager.destroy_vm(vm)
+        except:
+            logging.error("Failed to free vm %s", vm['name'])
+            vm['status'] = 'Fail'
+            raise
+        else:
+            del self.vms[name]
+            self._free_vm_resources(vm.pcis, vm.net_ifaces)
