353c89fb31f490de9015f905970e310a9789e06c
Solganik Alexander <alexanders@anyvision.co>
2019-12-30 23:55:25 +0200

    vm: refactor storage creation.
    This is a preparation commit to allow to create more than just one disk.
    In this commit we extract storage creation/deleting to its own method

--- lab/vms/vm_manager.py
+++ lab/vms/vm_manager.py
@@ -10,9 +10,23 @@ class VMManager(object):
         self.image_store = image_store
         self.thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=10)
 
-    async def allocate_vm(self, vm):
+    async def _create_storage(self, vm):
         image_path = await self.image_store.clone_qcow(vm['base_image'], vm['name'])
         vm['image'] = image_path
+
+    async def _delete_qcow_no_exception(self, image_path):
+        try:
+            await self.image_store.delete_qcow(image_path)
+        except:
+            logging.exception("Failed to delete image %s", image_path)
+
+    async def _delete_storage(self, vm):
+        if 'image' in vm:
+            await self._delete_qcow_no_exception(vm['image'])
+            del vm['image']
+
+    async def allocate_vm(self, vm):
+        await self._create_storage(vm)
         try:
             await self.loop.run_in_executor(self.thread_pool,
                                                      lambda: self.libvirt_api.define_vm(vm))
@@ -21,8 +35,7 @@ class VMManager(object):
         except:
             logging.error("Failed to create VM %s", vm)
             await self.destroy_vm(vm)
-            await self.image_store.delete_qcow(vm['image'])
-            del vm['image']
+            await self._delete_storage(vm)
             raise
         else:
             return vm
@@ -30,6 +43,5 @@ class VMManager(object):
     async def destroy_vm(self, vm):
         await self.loop.run_in_executor(self.thread_pool,
                                         lambda: self.libvirt_api.kill_by_name(vm["name"]))
-        await self.image_store.delete_qcow(vm['image'])
-        del vm['image']
+        await self._delete_storage(vm)
 
