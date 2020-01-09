5c104785525459ee8d22a47aea3a105f4aa1d36b
orihab <orielh@anyvision.co>
2020-01-09 18:55:19 +0200

    helpers: organized logging, no logic changes here

--- runner/helpers.py
+++ runner/helpers.py
@@ -5,12 +5,6 @@ from paramiko.ssh_exception import NoValidConnectionsError, AuthenticationExcept
 from infra.plugins.ssh import SSHCalledProcessError
 
 
-def init_logger():
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)',
-                        filename='output.log')
-
-
 def hardware_config(hardware):
     def wrapper(func):
         func.__hardware_reqs = hardware
@@ -38,19 +32,22 @@ def deploy_proxy_container(connected_ssh_module, auth_args=['password', 'root', 
     if removed:
         logging.warning("Unexpected behavior: removed proxy container despite that I shouldn't have needed to")
 
+    logging.info("initializing docker")
     run_cmd = f'{use_gravity_exec(connected_ssh_module)} docker run -d --rm --network=host --name=ssh_container orihab/ubuntu_ssh:2.0 {" ".join(auth_args)}'
-    res = connected_ssh_module.execute(run_cmd)
-    return res
+    connected_ssh_module.execute(run_cmd)
+    logging.info("docker is running")
 
 
 def remove_proxy_container(connected_ssh_module):
     try:
+        logging.info("trying to remove docker container")
         connected_ssh_module.execute(f'{use_gravity_exec(connected_ssh_module)} docker rm -f ssh_container')
         logging.info("removed successfully!")
         return True
     except SSHCalledProcessError as e:
         if 'No such container' not in e.stderr:
             raise e
+        logging.info("nothing to remove")
 
 
 def connect_via_running_docker(base):
@@ -58,38 +55,45 @@ def connect_via_running_docker(base):
         base.host.SSH.docker_connect(base.host.SSH.TUNNEL_PORT)
         return
     except NoValidConnectionsError:
-        print("no valid connections")
+        logging.exception("no valid connections")
     except socket.timeout as e:
-        print("socket timeout trying to connect via running docker")
+        logging.exception("socket timeout trying to connect via running docker")
     except AuthenticationException:
-        print("Authentication failed")
+        logging.exception("Authentication failed")
     except SSHException:
-        print("Error reading SSH protocol banner")
+        logging.exception("Error reading SSH protocol banner")
     except Exception as e:
-        print(f"caught other exception: {e}")
+        logging.exception(f"caught other exception: {e}")
     raise Exception("unsuccessful connecting via running docker...")
 
 
-def init_docker(host):
-    logging.info("initializing docker")
+def init_docker_and_connect(host):
+    logging.info(f"[{host}] connecting to ssh directly")
     host.SSH.connect()
+    logging.info(f"[{host}] connected successfully")
+
     deploy_proxy_container(host.SSH)
+
+    logging.info(f"[{host}] connecting to ssh container")
     host.SSH.docker_connect(host.SSH.TUNNEL_PORT)
-    logging.info("docker is running and ssh connected")
+    logging.info(f"[{host}] connected successfully")
 
 
 def init_dockers_and_connect(hosts):
     for name, host in hosts:
-        init_docker(host)
+        logging.info(f"[{name}: {host}] initializing machine ")
+        init_docker_and_connect(host)
+        logging.info(f"[{name}: {host}] success initializing docker and connecting to it")
 
 
 def tear_down_docker(host):
-    logging.info("tearing down docker")
     host.SSH.connect()
     remove_proxy_container(host.SSH)
-    logging.info("docker is stopped and disconnected")
 
 
 def tear_down_dockers(hosts):
     for name, host in hosts:
+        logging.info(f"[{name}: {host}] tearing down")
         tear_down_docker(host)
+        logging.info(f"[{name}: {host}] success tearing down")
+
