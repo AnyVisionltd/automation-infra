e79f9f614ed278fc700c79a9e26f6bc9389c5d3e
orihab <orielh@anyvision.co>
2020-01-09 16:19:16 +0200

    host.py: removed unnecessary params and improved init robustness

--- infra/model/host.py
+++ infra/model/host.py
@@ -10,7 +10,6 @@ host_config_example1 = {
     "ip": EXAMPLE_IP,
     "user": "user",
     "password": "pass",
-    "key_file_path": "",
     "alias": "monster",
     "host_id": 123,
     "host_type": "virtual",
@@ -20,7 +19,6 @@ host_config_example1 = {
 host_config_example2 = {
     "ip": EXAMPLE_IP,
     "user": "root",
-    "password": "",
     "key_file_path": "runner/docker_build/docker_user.pem",
     "alias": "monster",
     "host_id": 123,
@@ -28,22 +26,25 @@ host_config_example2 = {
     "allocation_id": ""
 }
 
+host_config_example3 = {
+    "ip": EXAMPLE_IP,
+    "user": "root",
+    "password": "pass",
+}
+
 
 class Host(object):
 
     def __init__(self, host_config):
-        assert (host_config.password and not host_config.key_file_path) or (
-                    not host_config.password and host_config.key_file_path), \
-            "password and key are mutually exclusive (password=%s, key=%s)" % (
-            host_config.password, host_config.key_file_path)
-        self.ip = host_config.ip
-        self.user = host_config.user
-        self.alias = host_config.alias
-        self.password = host_config.password
-        self.keyfile = host_config.key_file_path
-        self.id = host_config.host_id
-        self.type = host_config.host_type
-        self.allocation_id = host_config.allocation_id
+        _pem = host_config.pop('key_file_path', None)
+        _pass = host_config.pop('password', None)
+        assert (_pass and not _pem) or (_pem and not _pass), \
+            "password and key are mutually exclusive (password=%s, key=%s)" % (_pass, _pem)
+        self.ip = host_config.pop('ip')
+        self.user = host_config.pop('user')
+        self.password = _pass
+        self.keyfile = _pem
+        self.extra_config = host_config
         self.__plugins = {}
         self._temp_dir_counter = itertools.count()
 
@@ -80,27 +81,15 @@ def create_host(ip, u, p=None, pem_path=None):
         "ip": ip,
         "user": u,
         "password": p,
-        "key_file_path": pem_path,
-        "alias": "monster",
-        "host_id": 123,
-        "host_type": "virtual",
-        "allocation_id": ""
+        "key_file_path": pem_path
     }
     host = Host(Munch.fromDict(host_config))
     return host
 
 
 def test_functionality():
-    print("initializing host1...")
-    host1 = Host(Munch.fromDict(host_config_example1))
-    print(f"successful constructing {host1}")
-
-
-def init_example_hosts():
     host1 = Host(Munch.fromDict(host_config_example1))
     host2 = Host(Munch.fromDict(host_config_example2))
-    return [host1, host2]
-
-
-if __name__ == '__main__':
-    test_functionality()
+    host3 = Host(Munch.fromDict(host_config_example3))
+    host4 = create_host('0.0.0.0', 'user', p='pass')
+    host5 = create_host('0.0.0.0', 'user', pem_path='/path/to/pem')
