00950a320ecb8a1add5a45b26faac72d3e37ed83
orihab <orielh@anyvision.co>
2020-01-12 13:21:05 +0200

    tests: changed host alias so as not to confuse others

--- tests/test_automation_infra_plugin.py
+++ tests/test_automation_infra_plugin.py
@@ -6,10 +6,10 @@ import time
 from pytest_automation_infra.helpers import hardware_config
 
 
-@hardware_config(hardware={"ori_pass": {"gpu": 1, "ram": 16}, "ori_pem": {}})
+@hardware_config(hardware={"host1": {"gpu": 1, "ram": 16}, "host2": {}})
 def test_base_plugin_fixture(base_config):
-    logging.warning(f"PID of base_plugin_fixture: {os.getpid()}")
-    print(base_config.hosts.ori_pass)
-    print(f"inside test: successfully initialized hardware")
+    logging.info(f"PID of base_plugin_fixture: {os.getpid()}")
+    logging.info(base_config.hosts.host1)
+    logging.info(f"inside test: successfully initialized hardware")
     time.sleep(1)
     #assert False
