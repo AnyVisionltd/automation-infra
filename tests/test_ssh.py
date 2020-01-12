3f0b9a1777b2b4936464583ead96175fc94e4a70
orihab <orielh@anyvision.co>
2020-01-12 13:21:05 +0200

    tests: changed host alias so as not to confuse others

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -7,17 +7,17 @@ import pytest
 from pytest_automation_infra.helpers import hardware_config
 
 
-@hardware_config(hardware={"ori_pass": {"gpu": 1, "ram": 16}, "ori_pem": {}})
+@hardware_config(hardware={"host1": {"gpu": 1, "ram": 16}, "host2": {}})
 def test_ssh(base_config):
-    logging.warning(f"PID of test_Ssh: {os.getpid()}")
-    logging.info(f"Running ssh test on host {base_config.hosts.ori_pass.ip}")
+    logging.info(f"PID of test_Ssh: {os.getpid()}")
+    logging.info(f"Running ssh test on host {base_config.hosts.host1.ip}")
     os.system("echo this is a test > /tmp/temp.txt")
-    base_config.hosts.ori_pem.SSH.put('/tmp/temp.txt', '/tmp')
+    base_config.hosts.host1.SSH.put('/tmp/temp.txt', '/tmp')
     logging.info("put file!")
-    res = base_config.hosts.ori_pem.SSH.execute('ls /tmp')
+    res = base_config.hosts.host1.SSH.execute('ls /tmp')
     assert 'temp.txt' in res.split()
-    base_config.hosts.ori_pem.SSH.execute('rm /tmp/temp.txt')
-    res = base_config.hosts.ori_pem.SSH.execute('ls /tmp')
+    base_config.hosts.host1.SSH.execute('rm /tmp/temp.txt')
+    res = base_config.hosts.host1.SSH.execute('ls /tmp')
     logging.info("sleeping..")
     time.sleep(1)
     logging.info("woke up !")
