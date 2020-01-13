6d6ce9fb3e020d04b4dbfbab0b348e0b74e873e7
orihab <orielh@anyvision.co>
2020-01-13 11:14:27 +0200

    tests: minor changes for lastest infra changes

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -7,17 +7,17 @@ import pytest
 from pytest_automation_infra.helpers import hardware_config
 
 
-@hardware_config(hardware={"host1": {"gpu": 1, "ram": 16}, "host2": {}})
+@hardware_config(hardware={"host": {"gpu": 1, "ram": 16}, "host2": {}})
 def test_ssh(base_config):
     logging.info(f"PID of test_Ssh: {os.getpid()}")
-    logging.info(f"Running ssh test on host {base_config.hosts.host1.ip}")
+    logging.info(f"Running ssh test on host {base_config.hosts.host.ip}")
     os.system("echo this is a test > /tmp/temp.txt")
-    base_config.hosts.host1.SSH.put('/tmp/temp.txt', '/tmp')
+    base_config.hosts.host.SSH.put('/tmp/temp.txt', '/tmp')
     logging.info("put file!")
-    res = base_config.hosts.host1.SSH.execute('ls /tmp')
+    res = base_config.hosts.host.SSH.execute('ls /tmp')
     assert 'temp.txt' in res.split()
-    base_config.hosts.host1.SSH.execute('rm /tmp/temp.txt')
-    res = base_config.hosts.host1.SSH.execute('ls /tmp')
+    base_config.hosts.host.SSH.execute('rm /tmp/temp.txt')
+    res = base_config.hosts.host.SSH.execute('ls /tmp')
     logging.info("sleeping..")
     time.sleep(1)
     logging.info("woke up !")
