abce66672ef0546d98554571a305b2642ebacaf9
orihab <orielh@anyvision.co>
2020-01-13 11:14:27 +0200

    tests: minor changes for lastest infra changes

--- tests/test_automation_infra_plugin.py
+++ tests/test_automation_infra_plugin.py
@@ -6,10 +6,18 @@ import time
 from pytest_automation_infra.helpers import hardware_config
 
 
-@hardware_config(hardware={"host1": {"gpu": 1, "ram": 16}, "host2": {}})
+@hardware_config(hardware={"host": {"gpu": 1, "ram": 16}, "host2": {}})
 def test_base_plugin_fixture(base_config):
     logging.info(f"PID of base_plugin_fixture: {os.getpid()}")
-    logging.info(base_config.hosts.host1)
+    logging.info(base_config.hosts.host)
+    logging.info(base_config.hosts.host2)
     logging.info(f"inside test: successfully initialized hardware")
-    time.sleep(1)
-    #assert False
+    time.sleep(5)
+
+
+@hardware_config(hardware={"host3": {}})
+def test_base_plugin_fixture2(base_config):
+    logging.info(f"PID of base_plugin_fixture: {os.getpid()}")
+    logging.info(base_config.hosts.host3)
+    logging.info(f"inside test: successfully initialized hardware")
+    time.sleep(5)
