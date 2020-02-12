8f876a21968ab0cc92117d08763d9a98d7f1ae6d
Peter McConnell <peterm@anyvision.co>
2020-02-12 12:09:20 +0000

    pip: updating setup.py to load all packages and to use all requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import codecs
-from setuptools import setup
+import sys
+from setuptools import setup, find_packages
 
+if sys.version_info < (3, 6):
+    raise RuntimeError("automation-infra requires Python 3.6+")
+
+with open('README.md', mode='r', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('requirements3.txt', mode='r', encoding='utf-8') as f:
+    install_requires = f.read().splitlines()
 
 setup(
-    name='pytest-automation_infra',
+    name='automation_infra',
     author='Ori Haberman',
     author_email='orielh@anyvision.co',
     maintainer='Ori Haberman',
     description='plugin which inits hardware reqs of a test with pytest_runtest_setup hook',
-    py_modules=['pytest_automation_infra'],
-    install_requires=['pytest>=3.5.0'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=['tests']),
+    python_requires='>=3.6',
+    install_requires=install_requires,
     entry_points={
         'pytest11': [
             'automation_infra = pytest_automation_infra',
