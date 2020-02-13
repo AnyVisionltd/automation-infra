5c338c5794486e6f8f5ea4ce83d3f9738b76b246
raz <orih@anyvision.co>
2020-02-13 17:24:28 +0200

    Packer | Add (code) Snippet and SnippetRunner
    Snippet class creates an egg file from a callable target, and providing the
    instance that executes the code on remote host.
    
    SnippetRunner is the snippet executor. It clones the egg to the remote host
    and executes it with given args and kwargs. After the script is done parses
    and returns the output as a python object.
    
    The args and kwargs are added to the script execution command after pickled
    and converted to base64string, the main file of the egg knows to convert it
    back to python object. In the same way script's output is returned.
    
    The ssh plugin already has the code that integrated with the snippet logic,
    based on Sasha's old code. I intentionally barely touched this plugin.

---
+++ automation_infra/utils/snippet.py
@@ -0,0 +1,105 @@
+import os
+import base64
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+from .pypacker import PythonPacker
+
+
+class SnippetRunner(object):
+
+    def __init__(self, snippet, ssh, interpreter):
+        self.interpreter = interpreter
+        self.snippet = snippet
+        self.ssh = ssh
+        self.script_path = None
+
+    def run(self, *args, **kwargs):
+        self.deploy()
+        params = self._prepare_input(args, kwargs)
+        cmd = '{0} {1} {2}'.format(self.interpreter, self.script_path, params)
+        result = self.ssh.run_script(cmd)
+        return self._parse_result(result)
+
+    def run_background(self, *args, **kwargs):
+        self.deploy()
+        params = self._prepare_input(args, kwargs)
+        cmd = '{0} {1} {2}'.format(self.interpreter, self.script_path, params)
+        background = self.ssh.run_background_script(cmd)
+
+        def wait_result(timeout=None):
+            background.wait(timeout)
+            if background.running():
+                raise RuntimeError("script is still running")
+            return self._parse_result(background.output)
+
+        background.wait_result = wait_result
+        return background
+
+    def deploy(self):
+        if not self.snippet.archive_path:
+            self.snippet.prepare()
+        basename = os.path.basename(self.snippet.archive_path)
+        self.script_path = os.path.join('/tmp', basename)
+        self.ssh.put(self.snippet.archive_path, '/tmp')
+
+    @staticmethod
+    def _prepare_input(args, kwargs):
+        pickled_params = pickle.dumps((args, kwargs), pickle.HIGHEST_PROTOCOL)
+        return base64.b64encode(pickled_params).decode().strip()
+
+    @staticmethod
+    def _parse_result(result):
+        success, output = pickle.loads(base64.b64decode(result))
+        if not success:
+            raise output
+        return output
+
+
+class Snippet(object):
+
+    def __init__(self, target):
+        self.target = target
+        self.archive_path = None
+
+    _wrapper_script = """
+import sys
+import os
+import base64
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+def print_result(success, result):
+    output = pickle.dumps((success, result), protocol=pickle.HIGHEST_PROTOCOL)
+    output = base64.b64encode(output).decode().strip()
+    sys.stdout.write(output)
+    sys.stdout.flush()
+    exit(0)
+    
+try:
+    import {module}
+    if len(sys.argv) > 1:
+        args, kwargs = pickle.loads(base64.b64decode(sys.argv[1]))
+    else:
+        args, kwargs = tuple(), dict()
+    res = {module}.{target}(*args, **kwargs)
+except Exception as e:
+    print_result(False, e)
+else:
+    print_result(True, res)
+"""
+
+    def prepare(self, outfile=None):
+        module = self.target.__module__
+        name = self.target.__name__
+        script = self._wrapper_script.format(module=module, target=name)
+        packed = PythonPacker.from_script(script, outfile or name)
+        self.archive_path = packed.outfile
+
+    def create_instance(self, host, interpreter='python3'):
+        return SnippetRunner(self, host.SSH, interpreter)
