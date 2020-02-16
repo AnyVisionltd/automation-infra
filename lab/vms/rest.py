AnyVisionltd/automation-infra

commit 7dd20ad0ebe7faeb41b845621725702a898c4087

allocator: avoid double booking and double free for vms.
Currently there is no protection over creating same vm twice or freeing
same vm twice. This can happen if during some async operation such as
disk create or vm create another new/delete operation comes and process
is initiated for same VM. Especially this can put our GPU and NET
accounting out of sync with reality.
Solution is to protect async create/delete operations with lock so that
same vm could not be handled several times.

This fixes #42

lab/vms/rest.py
@@ -43,6 +43,8 @@ class HyperVisor(object):
         vm_name = request.match_info['name']
         try:
             await self.allocator.destroy_vm(vm_name)
+        except KeyError:
+            return web.json_response(status=404)
         except:
             logging.exception("Failed to destroy VM")
             return web.json_response({'status' : 'Failed'}, status=500)
@@ -65,10 +67,15 @@ class HyperVisor(object):
             return web.json_response(status=404)
 
         power_status = data['power']
-        if power_status == "on":
-            await self.allocator.vm_manager.start_vm(vm)
-        elif power_status == "off":
-            await self.allocator.vm_manager.stop_vm(vm)
+
+        async with vm.lock:
+            # double check after lock
+            if vm_name not in self.allocator.vms:
+                return web.json_response(status=404)
+            if power_status == "on":
+                await self.allocator.vm_manager.start_vm(vm)
+            elif power_status == "off":
+                await self.allocator.vm_manager.stop_vm(vm)
         return web.json_response({'status' : 'Success'}, status=200)
 
     async def handle_vm_status(self, request):
@@ -77,5 +84,9 @@ class HyperVisor(object):
         vm = self.allocator.vms.get(vm_name)
         if vm is None:
             return web.json_response(status=404)
-        info = await self.allocator.vm_manager.network_info(vm)
+        async with vm.lock:
+            # double check after lock
+            if vm_name not in self.allocator.vms:
+                return web.json_response(status=404)
+            info = await self.allocator.vm_manager.network_info(vm)
         return web.json_response({'info' : info}, status=200)

lab/vms/allocator.py
@@ -1,6 +1,7 @@
 import logging
 from lab import NotEnoughResourceException
 import munch
+import asyncio
 
 
 class Allocator(object):
@@ -81,26 +82,34 @@ class Allocator(object):
         vm = munch.Munch(name=vm_name, num_cpus=num_cpus, memsize=memory_gb,
                          net_ifaces=networks, sol_port=self._sol_port(),
                          pcis=gpus, base_image=base_image,
-                         disks=disks)
+                         disks=disks,
+                         lock=asyncio.Lock())
         self.vms[vm_name] = vm
 
-        try:
-            await self.vm_manager.allocate_vm(vm)
-        except:
-            self._free_vm_resources(gpus, networks)
-            del self.vms[vm_name]
-            raise
-        else:
-            logging.info("Allocated vm {vm}")
+        async with vm.lock:
+            try:
+                await self.vm_manager.allocate_vm(vm)
+            except:
+                self._free_vm_resources(gpus, networks)
+                del self.vms[vm_name]
+                raise
+            else:
+                logging.info(f"Allocated vm {vm}")
         return vm
 
     async def destroy_vm(self, name):
-        vm = self.vms[name]
-        try:
-            await self.vm_manager.destroy_vm(vm)
-        except:
-            logging.exception("Failed to free vm %s", vm['name'])
-            raise
-        else:
-            del self.vms[name]
-            self._free_vm_resources(vm.pcis, vm.net_ifaces)
+        vm = self.vms.get(name, None)
+        if not vm:
+            raise KeyError()
+        async with vm.lock:
+            # double check that vm is not yet deleted
+            if not name in self.vms:
+                raise KeyError()
+            try:
+                await self.vm_manager.destroy_vm(vm)
+            except:
+                logging.exception("Failed to free vm %s", vm['name'])
+                raise
+            else:
+                del self.vms[name]
+                self._free_vm_resources(vm.pcis, vm.net_ifaces)

commit 03f96664aa187be86de568dc03cbe0f774677f74

vms: correctly format json responce in list vms.
Currently list-vms rest api is broken by
Fixes: 7dd20ad ("allocator: avoid double booking and double free for vms.")

We added a lock to vm structure, however it cannot be marshalled to
json, and there was no reason to do so. In this fix we make all fields
that are marshalled in response to vms list fixed, to avoid this
faiulure.

lab/vms/rest.py
@@ -51,8 +51,18 @@ class HyperVisor(object):
         else:
             return web.json_response({'status' : 'Success'}, status=200)
 
+    def _jsonify_vms(self, vms):
+        return [{'name' : vm['name'],
+                 'cpus' : vm['num_cpus'],
+                 'memory' : vm['memsize'],
+                 'net' : vm['net_ifaces'],
+                 'pcis' : vm['pcis'],
+                 'image' : vm['image'],
+                 'disks' : vm['disks'],
+                 'status' : vm['status']} for vm in vms.values()]
+
     async def handle_list_vms(self, _):
-        return web.json_response({'vms' : self.allocator.vms}, status=200)
+        return web.json_response({'vms' : self._jsonify_vms(self.allocator.vms)}, status=200)
 
     async def handle_list_images(self, _):
         images = await self.image_store.list_images()