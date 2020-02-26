c2d65560f4263577554d259432559f68f64a38c2
Chen <camiel@anyvision.co>
2020-02-26 10:08:22 +0200

    infra | remove common bt files
    remove common bt files:
    1) execeptions
    2) assertions

--- infra/utils/assertions.py
+++
@@ -1,6 +0,0 @@
-from infra.utils.exceptions import ResponseStatusError
-
-
-def assert_status_code(code, response):
-    if response.status_code != code:
-        raise ResponseStatusError(response.status_code, response.json())
