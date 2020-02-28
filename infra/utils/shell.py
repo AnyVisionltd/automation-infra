81c160611537ecc46b5faf145918d92540f4f3ac
Solganik Alexander <alexanders@anyvision.co>
2020-02-28 10:48:20 +0200

    shell execute with string and array

--- infra/utils/shell.py
+++ infra/utils/shell.py
@@ -3,4 +3,6 @@ import logging
 
 def run_cmd(cmd, shell=False):
     logging.debug("running command %s", cmd)
+    if not shell and isinstance(cmd,str):
+        cmd = cmd.split()
     return subprocess.check_output(cmd, shell=shell).decode().strip()
