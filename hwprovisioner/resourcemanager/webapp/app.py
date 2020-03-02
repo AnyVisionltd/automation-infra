c78f8c1c365eb8f593345a11d86749ba13d96e5f
Peter McConnell <peterm@anyvision.co>
2020-03-02 16:33:53 +0000

    TMTNNFR-78: switching resourcemanager to new design

--- hwprovisioner/resourcemanager/webapp/app.py
+++ hwprovisioner/resourcemanager/webapp/app.py
@@ -5,8 +5,12 @@ python -m webapp.app serve
 """
 import weakref
 
+import asyncio
 import connexion
 
+from .listener import LISTENER
+from .processor import PROCESSOR
+from .config import CONFIG
 from .settings import log
 
 
@@ -22,10 +26,35 @@ def run_app(run=True, port=9080):
     cxapp.add_api(  # nosec
         "resourcemgr.yml", base_path="/api", pass_context_arg_name="request",
     )
+    cxapp.app.on_startup.append(start_daemons)
+    cxapp.app.on_cleanup.append(cleanup_daemons)
 
     if run:
         return cxapp.run()
     return cxapp.app
+
+
+async def start_daemons(app):
+    """
+    background tasks
+    """
+    # a single listener runs to volunteer
+    app['listen'] = app.loop.create_task(LISTENER.listen(app))
+
+    # we want a dedicated processor for each inventory item
+    app['process'] = []
+    for rtype in ["static", "dynamic"]:
+        for rref in CONFIG["resources"][rtype]:
+            app['process'].append(app.loop.create_task(PROCESSOR.process(rtype, rref)))
+
+
+async def cleanup_daemons(app):
+    """
+    application tidy ups
+    """
+    for proc in app['process']:
+        proc.cancel()
+    app['listen'].cancel()
 
 
 if __name__ == "__main__":
