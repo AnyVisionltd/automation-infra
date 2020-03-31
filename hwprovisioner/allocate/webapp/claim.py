9315a1c4940a159de01e990ae583b8cb549fb6c4
Peter McConnell <me@petermcconnell.com>
2020-03-31 20:20:57 +0100

    TMTNNFR-88: enriching jobs metadata on claim

--- hwprovisioner/allocate/webapp/claim.py
+++ hwprovisioner/allocate/webapp/claim.py
@@ -14,6 +14,15 @@ async def claim(request, body):
     """
     log.debug("got claim request %s", body)
     data = json.loads(body)
+    # updating job metadata
+    job = request.app["redis"].conn.hget("jobs", data["allocation_id"])
+    job_data = json.loads(job)
+    job_data["state"] = "claimed"
+    job_data["resourcemanager_id"] = data["resourcemanager_id"]
+    job_data["inventory_type"] = data["inventory_type"]
+    job_data["inventory_ref"] = data["inventory_ref"]
+    request.app["redis"].conn.hset("jobs", data["allocation_id"], json.dumps(job_data))
+    # adding connection info to dedicated job queue
     request.app["redis"].conn.publish(
         "j:%s" % data["allocation_id"],
         body,
