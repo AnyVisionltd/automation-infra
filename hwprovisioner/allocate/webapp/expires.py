beebe7bcd7aaec05e1eb9b08f56258f0b91ad983
Peter McConnell <me@petermcconnell.com>
2020-04-01 13:45:19 +0100

    TMTNNFR-88: removing no expiration field error messages due to noise

--- hwprovisioner/allocate/webapp/expires.py
+++ hwprovisioner/allocate/webapp/expires.py
@@ -16,7 +16,7 @@ async def expire(redis):
     jobs = redis.conn.hgetall("jobs")
     for job in jobs.values():
         data = json.loads(job)
-        try:
+        if "expiration" in data:
             if data["expiration"] <= time.time():
                 log.debug("found expired job! processing")
                 log.debug(data)
@@ -33,9 +33,5 @@ async def expire(redis):
                 log.debug("notified dedicated resource queue")
                 redis.conn.hdel("jobs", data["allocation_id"])
                 log.debug("deleted job")
-        except KeyError as err:
-            log.error("%s not found in jobs data", err)
-        except Exception as err:
-            log.error(err)
     await asyncio.sleep(10)
     await expire(redis)
