b35da99b33ffb549148f1be6127b1e5528f0f8e6
Peter McConnell <me@petermcconnell.com>
2020-04-02 11:11:06 +0100

    TMTNNFR-88: adding invalid content response (passes tests)

--- hwprovisioner/heartbeats/webapp/heartbeats.py
+++ hwprovisioner/heartbeats/webapp/heartbeats.py
@@ -13,10 +13,16 @@ async def heartbeat(request, body):
     accepts a heartbeat, extending the resource reservation for an
     allocation_id
     """
-    allocation_id = body["allocation_id"]
-    log.debug("received heartbeat from allocation_id %s", allocation_id)
-    await update_expires(request, allocation_id)
-    return web.json_response({"status": 200})
+    if "allocation_id" in body:
+        allocation_id = body["allocation_id"]
+        log.debug("received heartbeat from allocation_id %s", allocation_id)
+        await update_expires(request, allocation_id)
+        return web.json_response({"status": 200})
+    else:
+        return web.json_response(
+            {"status": 400, "reason": "'allocation_id' missing from body"},
+            status=400,
+        )
 
 
 async def update_expires(request, allocation_id):
