56c7fce100e550ccce47eecfd7643bf9a938bb38
oriHab <orielh@anyvision.co>
2020-04-20 20:36:07 +0300

    forward.py: pipeng disconnection bugfix
    There was a bug where if a service restarted, communication to all other
    services failed.
    This was because the forward server closed the ssh transport when it caught
    an exception opening a tunnel to a remote service, and the transport is
    shared between all tunnels.
    So.. fixed.

--- automation_infra/plugins/base_plugin.py
+++ automation_infra/plugins/base_plugin.py
@@ -17,13 +17,13 @@ class TunneledPlugin(object):
         if force_same_port:
             try:
                 self._forward_server, self.local_bind_port = forward.start_tunnel(
-                    remote, port, self._host.SSH, port)
+                    remote, port, self._host.SSH.get_transport(), port)
             except OSError:
                 self._forward_server, self.local_bind_port = forward.start_tunnel(remote, port,
-                                                                                  self._host.SSH)
+                                                                                  self._host.SSH.get_transport())
         else:
             self._forward_server, self.local_bind_port = forward.start_tunnel(remote, port,
-                                                                              self._host.SSH)
+                                                                              self._host.SSH.get_transport())
 
     def stop_tunnel(self):
         self._forward_server.shutdown()
