a4b2de824dacb4c86b362ac42015825db45b5713
oriHab <orielh@anyvision.co>
2020-04-27 15:30:13 +0300

    utils/requestor.py: interface to allow different types of communication
    The requestor object stamps out an interface which is to be implemented,
    in order to allow communication with services via different methods, ie
    tunneled, kong, or potentially others in the future.
    
    The requestor is to implement the CRUD methods.
    
    A typical way to use a requestor to communicate with a service can be as
    follows:
    
    requestor = Requestor()
    service = Service(requestor)
    res = service.method()
    
    where service is implemented like (pseudo):
    class Service(requestor):
        self.requestor = requestor
        def method():
            return self.requestor.get(route)

---
+++ automation_infra/utils/httprequestor.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+import requests
+
+
+class HTTPRequestor():
+    """The goal of the requestor object is to enable communication with a service in different ways.
+    The requestor needs to implement the CRUD methods.
+    Then the use would be as follows:
+    req = Requestor()
+    service = Service(req)
+    res = service.method()
+
+    where Service is defined something like:
+    def Service(requestor):
+        def __init__():
+            self.requestor = requestor
+
+        def method():
+            route = '/path/to/route'
+            res = self.requestor.get(route)
+            return res
+
+    The automation-infra implemented TunnelRequestor which enables interacting with a service via existing tunnel.
+    Other potential requestor could be a KongRequestor (to be implemented) or others as the need arises."""
+
+    @abstractmethod
+    def get(self, route, params=None, **kwargs) -> requests.Response:
+        """route is the path of the url without the domainname"""
+        pass
+
+    @abstractmethod
+    def post(self, route, data=None, json=None, **kwargs) -> requests.Response:
+        pass
+
+    @abstractmethod
+    def put(self, route, data=None, **kwargs) -> requests.Response:
+        pass
+
+    @abstractmethod
+    def delete(self, route, **kwargs) -> requests.Response:
+        pass
+
