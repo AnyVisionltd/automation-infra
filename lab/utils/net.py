AnyVisionltd/automation-infra

commit d71122402724dd7858775e592ac02c95ade5d7af

vms: allow hypervisor always to communicate with vm on bridged network.
Currently at some installations we encouter an issue that hypervisor
cannot communicate with vm when started in bridged mode.
This is caused by the fact that we dont manipulate with route metrics
for macvlan and hw interfaces. Here is more detailed explanation:

In bridged mode we use macvlan interface. macvlan allows multiple mac
addresses to coexist on same physical network. Now in order hypervisor
to be able to communicate with vm all network communication has to go
via macvlan device and not directly via hw device.
Now since we dont operate route table metrics we were not able to ensure
that, what happened is that in setup that hw device got lower metric it
did not work, on setup that hw device got higher metric it did work.
This is probably depends on distro and linux setup.

The solution is to manually setup metric on macvlan device and setup
slightly higher metric on hw device. This way all communication will go
via the macvlan interface and communication between hyprevisor and the
vm will work

lab/utils/net.py
@@ -65,12 +65,14 @@ def network_info(device_name):
 
 def setup_macvlan_device(eth_device, ip_iface, macvlan_device_name):
     logging.info("Going to setup macvlan device %s for device %s info %s", macvlan_device_name, eth_device, ip_iface)
+    macvlan_device_metric = 100
     try:
         add_macvlan_device(eth_device, macvlan_device_name)
         add_ip_to_device(macvlan_device_name, ip_iface.compressed)
         start_device(macvlan_device_name)
         delete_routes(macvlan_device_name)
-        add_route(ip_iface.network.compressed, macvlan_device_name)
+        add_route(ip_iface.network.compressed, macvlan_device_name, macvlan_device_metric)
+        change_route_metric_for_device(eth_device, macvlan_device_metric + 1)
     except:
         logging.exception("Failed to setup macvlan device %s on %s", macvlan_device_name, eth_device)
         if not device_exists(macvlan_device_name):

commit 6e169f1c3ff2b507b641599a367fb99dad25c64b

vms: prevent "jumpy" ssh connection on hypervisor start.
Currently we first introduce macvlan device, and and append a route to
it. With low metrics. this can directly cause communication to go via
macvlan device. Now later we change the metrics on hw ethernet device.
If that fails for some reason we "remove" macvlan device.
This is fine but when connected to hypervisor by ssh to perform initial
installation and something in this setup does not work we loose ssh
connection, services are restarted and this persists.
Solution is to move macvlan route append to the end when we are sure
that everything works and there is a little room for error. This will at
least reduce the amount of pain when something during installation goes
south.

Fixes: d711224 ("vms: allow hypervisor always to communicate with vm on bridged network.")

lab/utils/net.py
@@ -71,8 +71,8 @@ def setup_macvlan_device(eth_device, ip_iface, macvlan_device_name):
         add_ip_to_device(macvlan_device_name, ip_iface.compressed)
         start_device(macvlan_device_name)
         delete_routes(macvlan_device_name)
-        add_route(ip_iface.network.compressed, macvlan_device_name, macvlan_device_metric)
         change_route_metric_for_device(eth_device, macvlan_device_metric + 1)
+        add_route(ip_iface.network.compressed, macvlan_device_name, macvlan_device_metric)
     except:
         logging.exception("Failed to setup macvlan device %s on %s", macvlan_device_name, eth_device)
         if not device_exists(macvlan_device_name):