e4d67ef3d6d8eb952f03934250fe26cdcee2024c
oriHab <orielh@anyvision.co>
2020-04-28 12:38:10 +0300

    waiter: added time_limit context manager
    This function allows to add a timeout to potentially long (or hanging)
    function calls.

--- automation_infra/utils/waiter.py
+++ automation_infra/utils/waiter.py
@@ -1,4 +1,6 @@
+import signal
 import time
+from contextlib import contextmanager
 
 
 def wait_for_predicate_nothrow(predicate, timeout=10, interval=1.0, exception_cls=Exception):
@@ -34,4 +36,24 @@ def wait_nothrow(operation, timeout=10, interval=1.0):
         if time.time() - before > timeout:
             raise TimeoutError("Predicate timed out, during the time we caught theses exceptions: \n" + caught_exceptions)
         time.sleep(interval)
+
+
+@contextmanager
+def time_limit(seconds):
+    """
+    Enables a time_limit on function call. Use snippet:
+    try:
+        with waiter.time_limit(3):
+            long_call()
+    except TimeoutError:
+        print("timed out!")
+    """
+    def signal_handler(signum, frame):
+        raise TimeoutError("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
 
