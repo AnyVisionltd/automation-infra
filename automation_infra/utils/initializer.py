e0daa66c140bd78b6d74509ad2d14e11f99e76a9
oriHab <orielh@anyvision.co>
2020-04-28 12:51:07 +0300

    initializer: removed init of ResourceManager plugin
    The idea is to remove this plugin completely from this repo and move it
    to devops-automation-infra, but thats a significant breaking change, so
    at least for now lets remove this to enable using this infra without aws
    creds, ie clean_between_tests wont ping resource_manager and fail if no
    creds.

--- automation_infra/utils/initializer.py
+++ automation_infra/utils/initializer.py
@@ -1,10 +1,4 @@
-import logging
-
-from automation_infra.plugins.resource_manager import ResourceManager
-
-
 def init_plugins(host):
     """This method inits automation-infra plugins (if necessary) so that when
     host.clean_between_tests is called the plugins exist for the host.
     New plugins implemented in this repo should be added to this list."""
-    host.ResourceManager
