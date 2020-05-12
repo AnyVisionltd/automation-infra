c0248252a7645bc3783029ce70ef47e8fa210b42
oriHab <orielh@anyvision.co>
2020-05-12 12:51:21 +0300

    Revert "utils: add options to timeit decorator"
    This reverts commit 8d46221937e38ff57f777d129585120dfd5a9464.

--- automation_infra/utils/timer.py
+++ automation_infra/utils/timer.py
@@ -4,20 +4,17 @@ import time
 import logging
 
 
-
-def timeitdecorator(log_level=logging.INFO, min_time=1):
-    def _wrapper(func):
-        @wraps(func)
-        def _time_it(*args, **kwargs):
-            start = time.time()
-            try:
-                return func(*args, **kwargs)
-            finally:
-                duration = time.time() - start
-                if not min_time or duration > min_time:
-                    logging.log(log_level, f"{func}: execution time: {duration}s")
-        return _time_it
-    return _wrapper
+def timeitdecorator(func):
+    @wraps(func)
+    def _time_it(*args, **kwargs):
+        start = time.time()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            duration = time.time() - start
+            if duration > 1:
+                logging.debug(f"{func}: execution time: {duration}s")
+    return _time_it
 
 
 @contextmanager
