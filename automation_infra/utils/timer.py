74ea16228e3e119d2130015f6f0915ba3b449fda
Solganik Alexander <alexanders@anyvision.co>
2020-05-12 23:47:04 +0300

    timeit: allow to specify log level and min reported time.
    Fixes: 8d46221 ("utils: add options to timeit decorator")

--- automation_infra/utils/timer.py
+++ automation_infra/utils/timer.py
@@ -2,9 +2,14 @@ from contextlib import contextmanager
 from functools import wraps
 import time
 import logging
+import functools
 
 
-def timeitdecorator(func):
+
+def timeitdecorator(func=None, log_level=logging.INFO, min_time=1):
+    if func is None:
+        return functools.partial(timeitdecorator, log_level=log_level, min_time=min_time)
+
     @wraps(func)
     def _time_it(*args, **kwargs):
         start = time.time()
@@ -12,8 +17,8 @@ def timeitdecorator(func):
             return func(*args, **kwargs)
         finally:
             duration = time.time() - start
-            if duration > 1:
-                logging.debug(f"{func}: execution time: {duration}s")
+            if not min_time or duration > min_time:
+                logging.log(log_level, f"{func}: execution time: {duration}s")
     return _time_it
 
 
