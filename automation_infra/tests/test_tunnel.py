24b435f451afdc78d59338ba7bd9fc948b009e03
oriHab <orielh@anyvision.co>
2020-06-01 11:39:40 +0300

    test_tunnel: fix expected exception type

--- automation_infra/tests/test_tunnel.py
+++ automation_infra/tests/test_tunnel.py
@@ -9,7 +9,7 @@ from pytest_automation_infra.helpers import hardware_config
 @hardware_config(hardware={"host": {}})
 def test_tunnel(base_config):
     host = base_config.hosts.host
-    with pytest.raises(SSHException):
+    with pytest.raises(TimeoutError):
         failed_tunnel = host.TunnelManager.get_or_create("temp", "fake", 1234)
     consul_tunnel = host.TunnelManager.get_or_create("remote", host.ip, 22)
     logging.info(f"tunnel up on: {consul_tunnel.host_port}")
