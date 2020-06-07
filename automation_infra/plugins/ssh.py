9cc844addb55a8b9296bf6350e5fbaa7a94f5d7d
oriHab <orielh@anyvision.co>
2020-06-07 13:54:07 +0300

    host: refactored create_host into class_method

--- automation_infra/plugins/ssh.py
+++ automation_infra/plugins/ssh.py
@@ -1,4 +1,4 @@
-from infra import model
+from infra.model.host import Host
 from infra.model import plugins
 from automation_infra.plugins import connection
 from automation_infra.plugins.ssh_direct import SshDirect
@@ -8,7 +8,7 @@ class SSH(SshDirect):
     def connect(self, port=2222, timeout=10, user="root", password="pass"):
         # TODO: have handle security here
 
-        host = model.host.create_host(
+        host = Host.from_args(
             self._host.ip, user, password, None, port=port
         )
         self._connection = connection.Connection(host)
