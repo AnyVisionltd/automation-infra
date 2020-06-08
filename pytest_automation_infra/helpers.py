cf2259966420ea658d9d32608d2be5f92d8afb86
oriHab <orielh@anyvision.co>
2020-06-08 12:34:25 +0300

    helpers: assert automation_proxy doesnt exists bugfix
    What Im trying to assert here is that a container with the name
    automation_proxy doesnt exist, but docker ps will only show running containers.
    
    The fix is to do ps -a to show also non-running containers. Because we
    run docker run --rm there shouldnt be a container leftover when the
    conontainer stops it should be removed but I saw in some edge cases this
    can nonetheless happen.

--- pytest_automation_infra/helpers.py
+++ pytest_automation_infra/helpers.py
@@ -95,8 +95,8 @@ def set_up_docker_container(connected_ssh_module):
               f'--privileged ' \
               f'--network=host ' \
               f'--name=automation_proxy gcr.io/anyvision-training/automation-proxy:master'
-    assert not connected_ssh_module.execute("docker ps -q --filter 'name=automation_proxy'"), \
-        "you want to run automation_proxy but it is already running"
+    assert not connected_ssh_module.execute("docker ps -aq --filter 'name=automation_proxy'"), \
+        "you want to run automation_proxy but it already exists, please remove it beforehand!"
     try:
         connected_ssh_module.execute(run_cmd)
     except SSHCalledProcessError as e:
