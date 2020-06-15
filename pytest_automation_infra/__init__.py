f4c74db677fb5e394ee2c809bef532b58d47edfd
oriHab <orielh@anyvision.co>
2020-06-15 19:23:41 +0300

    download files on runtest_teardown
    For now I download (and delete afterwards) the folder /storage/logs
    which obv holds anyvision product docker logs.. in the future we may want
    to add more stuff to download?

--- pytest_automation_infra/__init__.py
+++ pytest_automation_infra/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import logging
+import re
 import threading
 import time
 from datetime import datetime
@@ -16,6 +17,7 @@ from infra.model.host import Host
 from automation_infra.plugins.ssh import SSH
 from automation_infra.plugins.ssh_direct import SshDirect
 from pytest_automation_infra import hardware_initializer, helpers
+from pytest_automation_infra.helpers import is_k8s
 
 
 class InfraFormatter(logging.Formatter):
@@ -306,3 +308,22 @@ def pytest_configure(config):
     config.option.log_format = log_fmt
     config.option.log_cli_date_format = date_fmt
     config.option.log_file_date_format = date_fmt
+
+
+def pytest_runtest_teardown(item):
+    base_config = item.funcargs['base_config']
+    if is_k8s(base_config.hosts.host.SshDirect):
+        # TODO: implement download_logs for k8s
+        return
+    dst = os.path.join(get_log_dir(item.config), 'docker_logs')
+    paths_to_download = ['/storage/logs/']
+    base_config.hosts.host.SshDirect.download(re.escape(dst), *paths_to_download)
+
+
+def get_log_dir(config):
+    handlers = logging.RootLogger.root.handlers
+    for handler in handlers:
+        if type(handler) == logging.FileHandler:
+            path = handler.baseFilename
+            return os.path.dirname(path)
+    return config.option.logger_logsdir
