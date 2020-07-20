AnyVisionltd/automation-infra

commit 0f424ad0ab070902777be303ec466e903fbb947f

pytest_automation_infra/try_initing_hosts_intelligently: moved intelligent logic to separate function
The problem with the current design was that the 'intelligent' part was
triggered only in the case of running a test in 'function' scope. If
running in 'session' scope, the pytest didnt know the hardware
requirements of each specific test (because it didnt happen yet), so if
there were 2 tests in a suite, which 1 required host1 and the other
called the req host2, the base_config.hosts would have the wrong keys.

The solution to this is to move the 'intelligent' logic out to a
function, and call it in the runtest_setup, that way we have access to
the tests requirements, and then we can update the base_config
accordingly.

This isnt a permanent solution, in fact running tests in session (or
module) scoped context is a compromise we are currently allowing because
setup can take a long time, but this will allow us to work for now.

pytest_automation_infra/__init__.py
@@ -201,44 +201,9 @@ def find_provisioner_hardware(request):
         return request.function.__initialized_hardware
 
 
-def try_initing_hosts_intelligently(request, hardware, base):
-    """This function tries matching initialized hardware with function hardware requirements intelligently. In the
-     provisioner case the matching is trivial. In the un-provisioner, the function matches hardware keys with
-     available keys, and then assigns the rest of the keys.
-     More detailed explanation:
-     If provisioner, hardware will hold each and every key in function hardware requirements, and therefore never enter
-     the first else (trivial case).
-     If running un-provisioner, hardware will hold the contents of the hardware.yaml file, which will have none/some/all
-     of the tests requirements (in terms of alias). So what the else does if the function key (required hardware)
-     isnt in hardware keys, takes a random key, if it isnt required by the test, matches it to the alias which the
-     function requires.
-     """
-    try:
-        for key in request.function.__hardware_reqs.keys():
-            if key in base.hosts.keys():
-                # already initialized, can happen because of the folllowing 'for' loop
-                continue
-            # This is the trivial case, the required key exists in the hardware:
-            if key in hardware.keys():
-                details = hardware[key]
-                base.hosts[key] = Host.from_args(key, **details)
-            else:
-                # This is the 'intelligent' part, trying to match keys from hardware.yaml to test reqs:
-                for name, details in hardware.items():
-                    if name in request.function.__hardware_reqs.keys():
-                        base.hosts[name] = Host.from_args(name, **details)
-                    else:
-                        base.hosts[key] = Host.from_args(key, **details)
-                        break
-
-                base.hosts[key] = Host.from_args(key, **details)
-
-    except AttributeError:
-        # This happens when running with module/session scope
-        for machine_name in hardware.keys():
-            base.hosts[machine_name] = Host.from_args(machine_name, **hardware[machine_name])
-    except KeyError as err:
-        raise Exception(f"not enough hosts defined in hardware.yaml to run test {request.function}")
+def init_hosts(hardware, base):
+    for machine_name in hardware.keys():
+        base.hosts[machine_name] = Host.from_args(machine_name, **hardware[machine_name])
 
 
 def start_heartbeat_thread(hardware, request):
@@ -263,7 +228,7 @@ def base_config(request):
         hb_thread = start_heartbeat_thread(hardware, request)
     base = DefaultMunch(Munch)
     base.hosts = Munch()
-    try_initing_hosts_intelligently(request, hardware, base)
+    init_hosts(hardware, base)
     helpers.init_proxy_containers_and_connect(base.hosts.items())
     logging.info("sucessfully initialized base_config fixture")
     yield base
@@ -288,16 +253,35 @@ def init_cluster_structure(base_config, cluster_config):
                 cluster[key] = val_list
 
 
+def match_base_config_hosts_with_hwreqs(hardware_reqs, base_config):
+    if len(hardware_reqs) > len(base_config.hosts):
+        raise Exception("Not enough hosts to fulfil test requirements")
+    for key in hardware_reqs.keys():
+        if key in base_config.hosts:
+            continue
+        else:
+            bc_host_keys = list(base_config.hosts.keys())
+            for host in bc_host_keys:
+                if host in hardware_reqs:
+                    continue
+                else:
+                    base_config.hosts[key] = base_config.hosts.pop(host)
+                    break
+        assert key in base_config.hosts
+    return base_config
+
+
 @pytest.hookimpl(hookwrapper=True, trylast=True)
 def pytest_runtest_setup(item):
     # The yield allows the base_config fixture to be init'ed:
     outcome = yield
     outcome.get_result()
     base_config = item.funcargs['base_config']
+    reqs = item.function.__hardware_reqs
+    item.funcargs['base_config'] = match_base_config_hosts_with_hwreqs(reqs, base_config)
     hosts = base_config.hosts.items()
     initializer.clean_infra_between_tests(hosts)
     init_cluster_structure(base_config, item.function.__cluster_config)
-
 
 
 def pytest_logger_fileloggers(item):
@@ -362,3 +346,5 @@ def get_log_dir(config):
             path = handler.baseFilename
             return os.path.dirname(path)
     return config.option.logger_logsdir
+
+

commit 140a463b39ff189e2a15304699f8ad82f6aa11e8

infra: fix host alias in "logs" to be presented as random number.
Currently host alias is initialized with random number (by mistake) in
case it does not present in hardware.yaml. This causes alias to be
garbage and not how the host used in test, which in case causes log
folder for that host to be called with "garbage" name.
In this fix we overwrite the alias when we initialize host structire for
the test.

Fixes: 0f424ad ("pytest_automation_infra/try_initing_hosts_intelligently: moved intelligent logic to separate function")

pytest_automation_infra/__init__.py
@@ -258,6 +258,7 @@ def match_base_config_hosts_with_hwreqs(hardware_reqs, base_config):
         raise Exception("Not enough hosts to fulfil test requirements")
     for key in hardware_reqs.keys():
         if key in base_config.hosts:
+            base_config.hosts[key].alias = key
             continue
         else:
             bc_host_keys = list(base_config.hosts.keys())
@@ -266,6 +267,7 @@ def match_base_config_hosts_with_hwreqs(hardware_reqs, base_config):
                     continue
                 else:
                     base_config.hosts[key] = base_config.hosts.pop(host)
+                    base_config.hosts[key].alias = key
                     break
         assert key in base_config.hosts
     return base_config