a6c0b7c4f1705e8c4e14acbde3989ce5bf6fc68a
oriHab <orielh@anyvision.co>
2020-07-22 12:24:57 +0300

    pluing/power.py: can power on/off a vm
    Is necessary for the HUT to be a vm otherwise will throw an exception.
    In addition, requires the resource_manager who "owns" the vm to be
    configured in the hardware.yaml.

---
+++ automation_infra/plugins/power.py
@@ -0,0 +1,40 @@
+import requests
+
+from automation_infra.plugins.ssh_direct import SshDirect
+from infra.model import plugins
+
+BASE_URL = "http://%s"
+
+class Power(object):
+    def __init__(self, host):
+        self._host = host
+        self.id = self._host.vm_id
+
+    def verify_available(self):
+        assert bool(self._host.SshDirect.execute('cat /proc/cpuinfo | grep -i hypervisor || [[ $? == 1 ]]', timeout=3).strip()), \
+            "Tried to power off a machine which isnt a vm"
+        try:
+            self.rm_ep = self._host.resource_manager_ep
+        except KeyError:
+            raise Exception("Please configure host's resource manager (in hardware.yaml) before using power plugin")
+        self.url = f'http://{self.rm_ep}/vms/{self.id}'
+
+    def off(self):
+        self.verify_available()
+        url = f'{self.url}/status'
+        res = requests.post(url, json={"power": "off"})
+        assert res.status_code == 200
+
+    def on(self):
+        url = f'{self.url}/status'
+        res = requests.post(url, json={"power": "on"})
+        assert res.status_code == 200
+
+    def status(self):
+        url = f'{self.url}'
+        res = requests.get(url)
+        assert res.status_code == 200
+        return res.json()['info']['status']
+
+
+plugins.register('Power', Power)
