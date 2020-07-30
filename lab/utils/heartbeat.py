6ebc0afaf0adde1810fa6bf4dc6d18144e9d2eee
oriHab <habermanori@gmail.com>
2020-07-30 15:45:49 +0300

    hwprovisioner/heartbeats.py: update heartbeats logic
    The allocations are now saved via hkey with key allocations instead of
    jobs. Much clearer that way.

--- lab/utils/heartbeat.py
+++ lab/utils/heartbeat.py
@@ -5,17 +5,20 @@ import logging
 import aiohttp
 
 
-async def send_heartbeats(rm_info):
+async def send_heartbeats(rm_info, allocator_ep):
     logging.debug(f'starting to send heartbeats with info: {rm_info}')
     while True:
-        rm_info['last_hb'] = int(time.time())
-        async with aiohttp.ClientSession() as client:
-            uri = "%s/api/resource_manager/heartbeat" % os.getenv("ALLOCATE_API", "http://localhost:8080")
-            logging.debug(f"sending heartbeat {rm_info} to url: {uri}")
-            async with client.put(
-                    uri, data=json.dumps(rm_info)) as resp:
-                data = await resp.json()
-                logging.debug(f"hb response: {data}")
-                if data['status'] != 200:
-                    logging.exception("sent hb failed, is allocator configure correctly?")
-                time.sleep(5)
+        try:
+            rm_info['last_hb'] = int(time.time())
+            async with aiohttp.ClientSession() as client:
+                uri = "http://%s/api/resource_manager/heartbeat" % allocator_ep
+                logging.debug(f"sending heartbeat {rm_info} to url: {uri}")
+                async with client.put(
+                        uri, data=json.dumps(rm_info)) as resp:
+                    data = await resp.json()
+                    logging.debug(f"hb response: {data}")
+                    if data['status'] != 200:
+                        logging.exception("sent hb failed, is allocator configure correctly?")
+                    time.sleep(5)
+        except:
+            continue
