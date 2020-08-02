251659ce62183a0744b7c48b6ba0ebdbc62dcec6
oriHab <habermanori@gmail.com>
2020-08-02 12:28:25 +0300

    allocator/expires.py: updated module to expire properly
    The changes to the allocate functionality required some changes to the
    expire functionality.

--- hwprovisioner/allocate/webapp/expires.py
+++ hwprovisioner/allocate/webapp/expires.py
@@ -5,33 +5,50 @@ import asyncio
 import json
 import time
 
+import aiohttp
+
+from . import fulfiller, rm_requestor
 from .settings import log
 
 
-async def expire(redis):
+async def deallocate(data):
+    for hardware in data['hardware_details']:
+        rm_ep = hardware['resource_manager_ep']
+        vm_name = hardware["vm_id"]
+        try:
+            await rm_requestor.deallocate(vm_name, rm_ep)
+        except:
+            log.exception(f"failed deallocating {vm_name} on ep {rm_ep}")
+            raise
+
+
+async def expire_allocations(redis):
     """
     looks for expired jobs in redis and processes them
     """
-    log.debug("processing expired jobs")
-    jobs = redis.conn.hgetall("jobs")
-    for job in jobs.values():
-        data = json.loads(job)
-        if "expiration" in data:
-            if data["expiration"] <= time.time():
-                log.debug("found expired job! processing")
-                log.debug(data)
-                # publish expired event to dedicated inventory queue
-                redis.conn.publish(
-                    "i:%s-%s-%s"
-                    % (
-                        data["resourcemanager_id"],
-                        data["inventory_type"],
-                        data["inventory_ref"],
-                    ),
-                    json.dumps({"expired_job": data}),
-                )
-                log.debug("notified dedicated resource queue")
-                redis.conn.hdel("jobs", data["allocation_id"])
-                log.debug("deleted job")
-    await asyncio.sleep(10)
-    await expire(redis)
+    conn = await redis.asyncconn
+    while True:
+        log.debug("processing expired jobs")
+        deallocate_tasks = dict()
+        allocations = await redis.allocations()
+        for allocation_id, allocation in allocations.items():
+            if "expiration" in allocation and allocation['status'] == 'success':
+                if allocation["expiration"] <= time.time():
+                    log.debug(f"found expired allocation {allocation['allocation_id']}! deallocating")
+                    allocation['status'] = 'deallocating'
+                    deallocate_tasks[allocation_id] = asyncio.ensure_future(deallocate(allocation))
+                    await conn.hset("allocations", allocation["allocation_id"], json.dumps(allocation))
+                else:
+                    log.debug(f"job {allocation['allocation_id']} ttl: {allocation['expiration'] - int(time.time())}")
+
+        for a_id, task in deallocate_tasks.items():
+            allocation = allocations[a_id]
+            try:
+                await task
+                await redis.delete("allocations", a_id)
+            except:
+                log.debug("exception deallocating")
+                allocation['status'] = 'error_deallocating'
+                await conn.hset("allocations", a_id, json.dumps(allocation))
+
+        await asyncio.sleep(10)
