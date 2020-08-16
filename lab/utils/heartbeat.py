96b035c2be034be325187ae73596deb137b2f200
orihab <orielh@anyvision.co>
2020-08-16 14:26:12 +0300

    Removed log message of send/receive heartbeat from hypervisor/allocator
    This is sent every few seconds and just junks up the important messages.

--- lab/utils/heartbeat.py
+++ lab/utils/heartbeat.py
@@ -12,11 +12,9 @@ async def send_heartbeats(rm_info, allocator_ep):
             rm_info['last_hb'] = int(time.time())
             async with aiohttp.ClientSession() as client:
                 uri = "http://%s/api/resource_manager/heartbeat" % allocator_ep
-                logging.debug(f"sending heartbeat {rm_info} to url: {uri}")
                 async with client.put(
                         uri, data=json.dumps(rm_info)) as resp:
                     data = await resp.json()
-                    logging.debug(f"hb response: {data}")
                     if data['status'] != 200:
                         logging.exception("sent hb failed, is allocator configure correctly?")
                     time.sleep(5)
