bffbaea5e8ae6af26d815d53882e9281a5a52509
oriHab <habermanori@gmail.com>
2020-08-30 18:09:17 +0300

    heartbeat: raise exception when receiving hb for deallocated machine
    Before this, the hb service didnt check the status of the allocation of
    the allocation_id it received a heartbeat for, just updated the
    expiration to 10 seconds later.
    This is improper functionality, so added logic on backend and client
    side which checks that the allocation is indeed succeeded (active)
    before returning success on heartbeat, and exiting the client side if
    the heartbeat was sent to an inactive allocation.

--- hwprovisioner/heartbeats/webapp/heartbeats.py
+++ hwprovisioner/heartbeats/webapp/heartbeats.py
@@ -16,7 +16,10 @@ async def heartbeat(request, body):
     if "allocation_id" in body:
         allocation_id = body["allocation_id"]
         log.debug("received heartbeat from allocation_id %s", allocation_id)
-        await update_expires(request, allocation_id)
+        try:
+            await update_expires(request, allocation_id)
+        except Exception as e:
+            return web.json_response({"status": 400, "reason": str(e)}, status=400)
         return web.json_response({"status": 200})
     else:
         return web.json_response(
@@ -33,5 +36,8 @@ async def update_expires(request, allocation_id):
     value = await conn.hget("allocations", allocation_id)
     d_value = json.loads(value)
     d_value["expiration"] = time.time() + 20
+    if d_value['status'] != 'success':
+        raise Exception(f"received hb for allocation which has status: {d_value['status']}")
     await conn.hset("allocations", allocation_id, json.dumps(d_value))
     log.debug("expiration extended")
+
