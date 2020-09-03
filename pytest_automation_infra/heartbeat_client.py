d3283c78fd6bd69c1b61ac41f1aafac14f35b20f
oriHab <habermanori@gmail.com>
2020-09-03 11:32:39 +0300

    heartbeat_client: wraps heartbeat api with object
    This module exposes all clientside heartbeat functionality

---
+++ pytest_automation_infra/heartbeat_client.py
@@ -0,0 +1,33 @@
+import os
+import threading
+import time
+
+import requests
+
+
+class HeartbeatClient(object):
+    def __init__(self, stop=lambda: False, ep=os.getenv('HABERTEST_HEARTBEAT_SERVER', "localhost:7080"), interval=2):
+        self.ep = ep
+        self.stop = stop
+        self.interval = interval
+
+    def send_heartbeat(self, allocation_id):
+        payload = {"allocation_id": allocation_id}
+        response = requests.post("http://%s/api/heartbeat" % self.ep, json=payload)
+        if response.status_code != 200:
+            raise KeyError("Error sending heartbeat: %s", response.json())
+
+    def send_heartbeats_until_stop(self, allocation_id):
+        while True:
+            if self.stop():
+                break
+            try:
+                self.send_heartbeat(allocation_id)
+            except KeyError:
+                os._exit(666)
+            time.sleep(self.interval)
+
+    def send_heartbeats_on_thread(self, allocation_id):
+        hb_thread = threading.Thread(target=self.send_heartbeats_until_stop, args=(allocation_id,), daemon=True)
+        hb_thread.start()
+        return hb_thread
