fe541af9d5b22920855c14bb25c702006ee189e6
Solganik Alexander <alexanders@anyvision.co>
2020-09-06 18:16:27 +0300

    ssh: move sshd file to separate entity instead of "seds" in dockerfile.
    Currently we are maintaining those updates as "sed" in docekrfile, this
    is very cumbersom. Instead lets just keep the copy of the file and copy
    it to inside container

--- docker_build/version.sh
+++ docker_build/version.sh
@@ -6,6 +6,7 @@ function docker_tag () {
     local dockerfile=${current_dir}/Dockerfile
 
     HASH_FILES=(${BASH_SOURCE[0]} $current_dir/requirements.txt $current_dir/docker_user.pem $current_dir/docker_user.pem.pub)
+    HASH_FILES+=($(find $current_dir/etc/ -type f))
     local files_sum="$(md5sum -- ${HASH_FILES[@]} | awk {'print $1'})"
     local total_sum="$(echo -n $files_sum | md5sum | awk '{print substr($1,1,12)}')"
     echo $total_sum
