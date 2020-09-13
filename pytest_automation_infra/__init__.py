fc38c12965e1ccdb43e459d70102f4e1b5c5f334
Solganik Alexander <alexanders@anyvision.co>
2020-09-13 09:13:48 +0300

    infra: add print to journal when test starts.
    Sometimes it is usefull to look at journal and understand what happened
    in linux during the test. For that we need a mark that test begun.
    Hence adding the mark to "clean between tests" procedure.

--- pytest_automation_infra/__init__.py
+++ pytest_automation_infra/__init__.py
@@ -241,6 +241,7 @@ def pytest_runtest_setup(item):
         logging.info(f"\n\n{host.SshDirect.ssh_string} # host \n{host.SSH.ssh_string} # container\n\n")
     logging.debug("cleaning between tests..")
     initializer.clean_infra_between_tests(hosts)
+    initializer.clean_infra_between_tests(hosts, item.function.__name__)
     init_cluster_structure(base_config, item.function.__cluster_config)
     logging.debug("done runtest_setup")
     logging.debug("\n-----------------runtest call---------------\n")
