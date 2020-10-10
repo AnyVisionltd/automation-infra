7d4363edabe417024d8bfe1a9f4267dac2b62f18
Solganik Alexander <alexanders@anyvision.co>
2020-10-10 11:35:24 +0300

    rpyc: make rpyc permissive for pickels and attributes.
    We dont want security, we want access for all

--- automation_infra/utils/rpyc_service.py
+++ automation_infra/utils/rpyc_service.py
@@ -6,5 +6,7 @@ from rpyc.utils.server import ThreadedServer
 def start_service(port, loglevel, service):
     ch = logging.StreamHandler(sys.stdout)
     logging.basicConfig(level=loglevel, format='%(relativeCreated)6d %(threadName)s %(message)s', handlers=[ch])
-    server = ThreadedServer(service, hostname="0.0.0.0", port=port, protocol_config={"allow_public_attrs": True})
+    server = ThreadedServer(service, hostname="0.0.0.0", port=port, protocol_config={"allow_public_attrs": True,
+                                                                                     'allow_setattr' : True,
+                                                                                     "allow_pickle": True})
     server.start()
