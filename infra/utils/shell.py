c83757e861bdfd4ce64012ef6e6fba124ac378b0
oriHab <habermanori@gmail.com>
2020-10-15 13:57:24 +0300

    infra-backend: removed all relevant files/folders
    We decided the backed will be moved to its own repo by the name of
    habertest-backend.

--- infra/utils/shell.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import logging
-
-def run_cmd(cmd, shell=False):
-    logging.debug("running command %s", cmd)
-    if not shell and isinstance(cmd,str):
-        cmd = cmd.split()
-    return subprocess.check_output(cmd, shell=shell).decode().strip()
