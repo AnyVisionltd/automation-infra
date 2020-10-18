5313b326acf4694e870f752893bbe835787c6fba
Solganik Alexander <alexanders@anyvision.co>
2020-10-18 16:52:35 +0300

    rpyc: remove all security stuff

--- automation_infra/utils/rpyc_service.py
+++ automation_infra/utils/rpyc_service.py
@@ -6,5 +6,8 @@ from rpyc.utils.server import ThreadedServer
 def start_service(port, loglevel, service):
     ch = logging.StreamHandler(sys.stdout)
     logging.basicConfig(level=loglevel, format='%(relativeCreated)6d %(threadName)s %(message)s', handlers=[ch])
-    server = ThreadedServer(service, hostname="0.0.0.0", port=port, protocol_config={"allow_public_attrs": True})
+    server = ThreadedServer(service, hostname="0.0.0.0", port=port, protocol_config={"allow_public_attrs": True,
+                                                                                     "allow_getattr" : True,
+                                                                                     "allow_all_attrs": True,
+                                                                                     "allow_pickle" : True})
     server.start()
