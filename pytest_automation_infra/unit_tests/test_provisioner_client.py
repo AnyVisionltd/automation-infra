ba681762e8bfb25872486197cf21eebd47fd2a98
oriHab <orielh@anyvision.co>
2020-11-26 15:16:13 +0200

    test_provisioner_client.py: updated test to catch hb bug
    Im not 100% sure why the test worked before this fixed as the stop
    function wasnt defined properly, but now the test verifies complete
    control over heartbeat thread via stop function delegate.

--- pytest_automation_infra/unit_tests/test_provisioner_client.py
+++ pytest_automation_infra/unit_tests/test_provisioner_client.py
@@ -2,6 +2,7 @@
 Can be run like: python -m pytest pytest_automation_infra/unit_tests/test_provisioner_client.py -s --log-cli-level=info
 """
 import logging
+import threading
 import time
 
 import pytest
@@ -11,19 +12,29 @@ from pytest_automation_infra import provisioner_client, heartbeat_client
 
 def test_init_hardware_happy_flow():
     provisioner = provisioner_client.ProvisionerClient()
-    stop = False
-    hb = heartbeat_client.HeartbeatClient(lambda: stop)
+    stop = threading.Event()
+    hb = heartbeat_client.HeartbeatClient(stop)
     req = {"host": {}}
     hardware = provisioner.provision(req)
     assert hardware
     allocation_id = hardware['allocation_id']
     logging.info(f"provisioned hardware successfully: {hardware}")
+    logging.info("starting heartbeat")
     hb_thread = hb.send_heartbeats_on_thread(allocation_id)
+    time.sleep(3)
+    assert hb_thread.is_alive()
+    logging.info("stopping heartbeat")
+    stop.set()
+    time.sleep(3)
+    assert not hb_thread.is_alive()
+    logging.info("starting heartbeat")
+    stop.clear()
+    hb_thread = hb.send_heartbeats_on_thread(allocation_id)
+    time.sleep(3)
+    assert hb_thread.is_alive()
     time.sleep(30)
     logging.info("stopping heartbeat")
-    stop = True
-    time.sleep(3)
-    assert not hb_thread.isAlive()
+    stop.set()
     logging.info("releasing hardware")
     provisioner.release(allocation_id)
     allocations = provisioner.allocations()
@@ -42,9 +53,9 @@ def test_init_hardware_happy_flow():
     hb_thread = hb.send_heartbeats_on_thread(allocation_id)
     time.sleep(5)
     logging.info("stopping heartbeats")
-    stop = True
+    stop.set()
     time.sleep(3)
-    assert not hb_thread.isAlive()
+    assert not hb_thread.is_alive()
     logging.info("waiting for expiration")
     time.sleep(35)
     allocations = provisioner.allocations()
