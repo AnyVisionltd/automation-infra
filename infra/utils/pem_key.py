a1681ae1173d1723d385d55348f730e5a4aa8aa6
oriHab <orielh@anyvision.co>
2020-12-02 18:30:38 +0200

    infra/utils/pem_key: added to_string method

--- infra/utils/pem_key.py
+++ infra/utils/pem_key.py
@@ -6,3 +6,10 @@ def from_string(pkey_string):
     file_obj = io.StringIO(pkey_string)
     return paramiko.RSAKey.from_private_key(file_obj)
 
+
+def to_string(pkey):
+    file_obj = io.StringIO()
+    pkey.write_private_key(file_obj)
+    file_obj.seek(0)
+    content = file_obj.read()
+    return content
