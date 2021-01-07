04035bb6ba54d2094cbfeffb8fc85bbb6d41c727
oriHab <orielh@anyvision.co>
2021-01-07 23:42:40 +0200

    pytest_automation_infra: removed a ton of logic which was moved to other plugins
    This used to be the automation-infras main plugin, with a ton of logic
    and flow control statement. Now that the logic was separated out to
    other plugins, this plugin is much smaller, simpler, and easier to
    maintain.

--- pytest_automation_infra/__init__.py
+++ pytest_automation_infra/__init__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
+import json
 import os
 import logging
 import re
 import subprocess
 import signal
-from datetime import datetime
 
 import pytest
 import yaml
@@ -15,16 +14,6 @@ from automation_infra.utils import initializer, concurrently
 from infra.model.host import Host
 from pytest_automation_infra import helpers
 
-from automation_infra.plugins.ssh import SSH
-from automation_infra.plugins.ssh_direct import SshDirect
-
-
-class InfraFormatter(logging.Formatter):
-    def __init__(self):
-        msg_fmt = "%(asctime)20.20s.%(msecs)d %(threadName)-10.10s %(levelname)-6.6s %(message)-75s " \
-              "%(funcName)-15.15s %(pathname)-70s:%(lineno)4d"
-        logging.Formatter.__init__(self, msg_fmt, datefmt='%Y-%m-%d %H:%M:%S')
-
 
 def pytest_addoption(parser):
     parser.addoption("--fixture-scope", type=str, default='session', choices={"function", "module", "session"},
@@ -33,7 +22,6 @@ def pytest_addoption(parser):
                      help="path to hardware_yaml")
     parser.addoption("--extra-tests", action="store", default="",
                      help="tests to run in addition to specified tests and marks. eg. 'test_sanity.py test_extra.py'")
-    parser.addoption("--logs-dir", action="store", default="", help="custom directory to store logs in")
     parser.addoption("--provisioned-hardware", type=str)
 
 
@@ -47,7 +35,6 @@ def get_local_config(local_config_path):
         raise Exception("""local hardware_config yaml not found""")
     with open(local_config_path, 'r') as f:
         local_config = yaml.full_load(f)
-    logging.info(f"local_config: {local_config}")
     return local_config
 
 
@@ -78,10 +65,6 @@ def pytest_collection_modifyitems(session, config, items):
             item.add_marker(pytest.mark.extra)
 
 
-def determine_scope(fixture_name, config):
-    return config.getoption("--fixture-scope")
-
-
 def configured_hardware(request):
     return getattr(request.session, "__initialized_hardware", None) \
            or getattr(request.function, "__initialized_hardware", None)
@@ -100,20 +83,22 @@ def init_base_config(hardware):
         if host.pkey:
             host.add_to_ssh_agent()
     for name, host in base.hosts.items():
-        logging.debug(f"[{host}] connecting to ssh directly")
+        logging.info(f"[{host}] waiting for ssh connection...")
         host.SshDirect.connect(timeout=60)
-        logging.debug(f"[{host}] connected successfully")
+        host.SshDirect.execute("mkdir -p -m 777 /tmp/automation_infra")
+        logging.info(f"[{host}] success!")
     return base
 
 
-@pytest.fixture(scope=determine_scope)
+@pytest.fixture()
 def base_config(request):
     logging.debug("\n<---------------------initing base_config fixture------------------>\n")
     hardware = configured_hardware(request)
     assert hardware, "Didnt find configured_hardware in base_config fixture..."
     base = init_base_config(hardware)
     logging.debug("\n<-----------------sucessfully initialized base_config fixture------------>\n")
-    request.config.hook.pytest_after_base_config(base_config=base)
+    # Deploy proxy container (if pytest_devops_infra is invoked)
+    request.config.hook.pytest_after_base_config(base_config=base, request=request)
     return base
 
 
@@ -153,11 +138,9 @@ def match_base_config_hosts_with_hwreqs(hardware_reqs, base_config):
 
 @pytest.hookimpl(hookwrapper=True, trylast=True)
 def pytest_runtest_setup(item):
-    configure_item_loghandler(item)
     logging.debug(f"\n<---------runtest_setup {'.'.join(item.listnames()[-2:])}---------------->\n")
     configured_hw = configured_hardware(item._request)
     if not configured_hw:
-        logging.info("getting locally configured hardware")
         hardware = dict()
         hardware['machines'] = get_local_config(item.config.getoption("--hardware"))
         item.session.__initialized_hardware = hardware
@@ -165,7 +148,7 @@ def pytest_runtest_setup(item):
     hardware = configured_hardware(item._request)
     assert hardware, "Couldnt find configured hardware in pytest_runtest_setup"
     first_machine = next(iter(hardware['machines'].values()))
-    hut_conn_format = "HUT connection string:\n\n {} \n\n"
+    hut_conn_format = "HUT connection string:\n\n{}\n\n"
     if first_machine['password']:
         conn_string = f"sshpass -p {next(iter(hardware['machines'].values()))['password']} ssh -o StrictHostKeyChecking=no {next(iter(hardware['machines'].values()))['user']}@{next(iter(hardware['machines'].values()))['ip']}"
     else:
@@ -194,101 +177,17 @@ def pytest_runtest_setup(item):
     logging.debug("\n-----------------runtest call---------------\n")
 
 
-def pytest_logger_fileloggers(item):
-    return [('', logging.INFO)]
-
-
-def configure_item_loghandler(item):
-    config = item.config
-    log_dir = os.path.join(config.option.logger_logsdir, _sanitize_nodeid(item.nodeid))
-    os.environ["TEST_LOG_DIR"] = log_dir
-    os.makedirs(log_dir, exist_ok=True)
-    debug_fh = logging.FileHandler(f'{log_dir}/infra.log', mode='w')
-    debug_fh.setLevel(logging.DEBUG)
-    debug_fh.setFormatter(logging.Formatter(config.option.log_format))
-    item.log_handler = debug_fh
-    logging.getLogger().addHandler(item.log_handler)
-
-
-def pytest_logger_logsdir(config):
-    return config.option.logger_logsdir
-
-
-def pytest_logger_config(logger_config):
-    logger_config.split_by_outcome()
-    logger_config.set_formatter_class(InfraFormatter)
-
-
-def configure_logging(config):
-    config.option.log_format = '%(asctime)s.%(msecs)0.3d %(threadName)-10.10s %(levelname)-6.6s %(message)s %(funcName)-15.15s %(pathname)s:%(lineno)d'
-    config.option.log_cli_format = config.option.log_format
-
-    config.option.log_file_date_format = '%Y-%m-%d %H:%M:%S'
-    config.option.log_cli_date_format = config.option.log_file_date_format
-    custom_logs_dir = config.getoption("--logs-dir")
-    session_logs_dir = custom_logs_dir if custom_logs_dir else f'logs/{datetime.now().strftime("%Y_%m_%d__%H%M_%S")}'
-
-    infra_logs_dir = f'{session_logs_dir}/infra_logs'
-    os.makedirs(infra_logs_dir, exist_ok=True)
-
-    tests_logs_dir = f'{session_logs_dir}/tests_logs'
-    os.makedirs(tests_logs_dir, exist_ok=True)
-
-    config.option.logger_logsdir = tests_logs_dir
-
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
-    log_fmt = config.option.log_format
-
-    debug_fh = logging.FileHandler(f'{infra_logs_dir}/debug.log', mode='w')
-    debug_fh.setLevel(logging.DEBUG)
-    debug_fh.setFormatter(logging.Formatter(log_fmt))
-    root_logger.addHandler(debug_fh)
-
-    info_fh = logging.FileHandler(f'{infra_logs_dir}/info.log', mode='w')
-    info_fh.setLevel(logging.INFO)
-    info_fh.setFormatter(logging.Formatter(log_fmt))
-    root_logger.addHandler(info_fh)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(logging.Formatter(log_fmt))
-    root_logger.addHandler(ch)
-
-
-def pytest_configure(config):
-    configure_logging(config)
-
-
 def organize_remote_logs(ssh_direct):
     ssh_direct.execute('sudo sh -c "journalctl > /tmp/journal.log"')
 
 
-def download_host_logs(host, logs_dir):
+def download_host_logs(host, logs_dir, download_logs_hook=None):
     dest_dir = os.path.join(logs_dir, host.alias)
     os.makedirs(dest_dir, exist_ok=True)
     host.SshDirect.execute('sudo sh -c "journalctl > /tmp/journal.log"')
     host.SshDirect.download(dest_dir, '/tmp/journal.log')
-
-
-def _sanitize_nodeid(filename):
-    filename = filename.replace('::()::', '/')
-    filename = filename.replace('::', '/')
-    filename = re.sub(r'\[(.+)\]', r'-\1', filename)
-    return filename
-
-
-def pytest_report_teststatus(report, config):
-    if report.longreprtext:
-        logging.info(report.longreprtext)
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    result = outcome.get_result()
-    if result.when == 'call':
-        logging.info(f"\n>>>>>>>>>>{'.'.join(item.listnames()[-2:])} {'PASSED' if result.passed else 'FAILED'} {f' -> {call.excinfo.value}' if not result.passed else ''}")
+    if download_logs_hook:
+        download_logs_hook(host=host, dest_dir=dest_dir)
 
 
 def pytest_runtest_teardown(item):
