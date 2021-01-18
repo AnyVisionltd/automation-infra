43f03692f503d62c0cff01006fb3af18467266e6
oriHab <orielh@anyvision.co>
2021-01-18 11:58:18 +0200

    test_tunnel.py: moved to devops-infra

--- automation_infra/tests/basic_tests/test_tunnel.py
+++
@@ -1,9 +0,0 @@
-import logging
-from pytest_automation_infra.helpers import hardware_config
-
-
-@hardware_config(hardware={"host": {}})
-def test_tunnel(base_config):
-    host = base_config.hosts.host
-    consul_tunnel = host.TunnelManager.get_or_create("remote", host.ip, 22)
-    logging.info(f"tunnel up on: {consul_tunnel.host_port}")
