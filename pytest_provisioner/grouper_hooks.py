07d43b76df41bab7da5960248ad3209cd54e994b
oriHab <orielh@anyvision.co>
2021-01-25 10:12:22 +0200

    pytest_can_run_together: only run together tests which are in the same folder
    This is important for 2 reasons:
    1. there could be different conftest.py
    files in diff folders, which would trigger different installations.
    2. We will want jenkins to run many core tests together and therefore we
       will want a run command something like
       python -m pytest /core-product/tests /camera-service/tests /licensing-service/tests
       --num-parallel 3
       and then for the different folders to run in parallel. In order to do
       this we will need to put the run command in 1 Jenkins stage, so we
       would like the group the tests by service.

--- pytest_provisioner/grouper_hooks.py
+++ pytest_provisioner/grouper_hooks.py
@@ -1,4 +1,6 @@
 import logging
+import os
+
 import pytest
 
 import pytest_provisioner
@@ -10,10 +12,12 @@ def pytest_can_run_together(item1, item2):
     This is the trivial implementation if no one else implemented this hook.
     It does a trivial comparison of hardware_reqs adn cluster_config.
     """
-    if item1.function.__hardware_reqs == item2.function.__hardware_reqs and \
-            item2.function.__cluster_config == item2.function.__cluster_config:
-        return True
-    return False
+    if os.path.split(item1.fspath)[0] != os.path.split(item2.fspath)[0]:
+        return False
+    if item1.function.__hardware_reqs != item2.function.__hardware_reqs or \
+            item2.function.__cluster_config != item2.function.__cluster_config:
+        return False
+    return True
 
 
 def pytest_finished_handling_group(session, worker):
