ac37ebad997ecf68940a2c92a399b3817c97e460
Solganik Alexander <alexanders@anyvision.co>
2021-03-07 20:27:41 +0200

    tests: remove tests irrelevant for the project

--- automation_infra/tests/k8s_tests/yaml_files/deploy_cluster.sh
+++
@@ -1,7 +0,0 @@
-cd $(dirname $0)
-rm kube_config_cluster.yml cluster.rkestate
-
-rke up
-kubectl --kubeconfig kube_config_cluster.yml apply -f hello-deployment.yaml
-kubectl --kubeconfig kube_config_cluster.yml rollout status deployment/hello
-kubectl --kubeconfig kube_config_cluster.yml apply -f hello-svc.yaml
